d5d17a560c751e560a0d8b37d5651b1d8778b9cd
Carlos Perez <carlos_perez@darkoperator.com>
2026-06-27 21:29:25 -0400

    test: cover sysmon completion helpers

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,42 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+export const CONDITION_COMPLETIONS = [
+	'is',
+	'is not',
+	'is any',
+	'contains',
+	'contains any',
+	'contains all',
+	'excludes',
+	'excludes any',
+	'excludes all',
+	'begin with',
+	'not begin with',
+	'end with',
+	'not end with',
+	'less than',
+	'more than',
+	'image'
+];
+
+export const ONMATCH_COMPLETIONS = [
+	'include',
+	'exclude'
+];
+
+export function getAttributeCompletions(linePrefix: string): string[] | undefined {
+	if (linePrefix.endsWith('condition="')) {
+		return CONDITION_COMPLETIONS;
+	}
+
+	if (linePrefix.endsWith('onmatch="')) {
+		return ONMATCH_COMPLETIONS;
+	}
+
+	return undefined;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,5 +1,10 @@
 import * as assert from 'assert';
 import * as path from 'path';
+import {
+	CONDITION_COMPLETIONS,
+	ONMATCH_COMPLETIONS,
+	getAttributeCompletions
+} from '../../extension';
 
 const packageJson = require(path.join(__dirname, '../../../package.json'));
 
@@ -13,5 +18,55 @@ suite('Extension Metadata', () => {
 
 		assert.ok(language, 'Expected smc language contribution');
 		assert.deepStrictEqual(language.extensions, ['.smc']);
+	});
+});
+
+suite('Completion Helpers', () => {
+	test('condition completions include current Sysmon operators', () => {
+		assert.deepStrictEqual(CONDITION_COMPLETIONS, [
+			'is',
+			'is not',
+			'is any',
+			'contains',
+			'contains any',
+			'contains all',
+			'excludes',
+			'excludes any',
+			'excludes all',
+			'begin with',
+			'not begin with',
+			'end with',
+			'not end with',
+			'less than',
+			'more than',
+			'image'
+		]);
+	});
+
+	test('onmatch completions include include and exclude', () => {
+		assert.deepStrictEqual(ONMATCH_COMPLETIONS, [
+			'include',
+			'exclude'
+		]);
+	});
+
+	test('returns condition completions after condition attribute prefix', () => {
+		assert.deepStrictEqual(
+			getAttributeCompletions('<Image condition="'),
+			CONDITION_COMPLETIONS
+		);
+	});
+
+	test('returns onmatch completions after onmatch attribute prefix', () => {
+		assert.deepStrictEqual(
+			getAttributeCompletions('<ProcessCreate onmatch="'),
+			ONMATCH_COMPLETIONS
+		);
+	});
+
+	test('returns no completions outside supported attribute prefixes', () => {
+		assert.strictEqual(getAttributeCompletions('<Image name="'), undefined);
+		assert.strictEqual(getAttributeCompletions('<Image condition="value'), undefined);
+		assert.strictEqual(getAttributeCompletions('<Image>'), undefined);
 	});
 });
